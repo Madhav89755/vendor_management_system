e4f47fa0ecd43d85462e4c7f57375e091983992c
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2024-04-27 09:00:18 +0000

    serializers added

---
+++ proj_root/vendor/serializers.py
@@ -0,0 +1,27 @@
+""" Serializer class for the Models """
+from rest_framework import serializers
+from .models import *
+
+class VendorModelSerializer(serializers.ModelSerializer):
+    """ Vendor Model Serializer """
+    class Meta:
+        """ Meta class """
+        model=Vendor
+        fields='__all__'
+        read_only_fields=['id', 'created_on', 'updated_on']
+
+class PurchaseOrderModelSerializer(serializers.ModelSerializer):
+    """ PurchaseOrder Model Serializer """
+    class Meta:
+        """ Meta class """
+        model=PurchaseOrder
+        fields='__all__'
+        read_only_fields=['po_number', 'created_on', 'updated_on']
+
+class HistoricalPerformanceModelSerializer(serializers.ModelSerializer):
+    """ PurchaseOrder Model Serializer """
+    class Meta:
+        """ Meta class """
+        model=HistoricalPerformance
+        fields='__all__'
+        read_only_fields=['id', 'created_on', 'updated_on']
